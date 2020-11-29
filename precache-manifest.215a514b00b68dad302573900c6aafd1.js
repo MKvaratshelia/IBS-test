496236624c5ba5382afa8b3c19fc466e4cc3e902
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2020-11-29 18:23:49 +0300

    Updates

---
+++ precache-manifest.215a514b00b68dad302573900c6aafd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a73043d44fab3b9bdc27057e20076f",
+    "url": "/IBS-test/index.html"
+  },
+  {
+    "revision": "f6470c88bac13f698ebd",
+    "url": "/IBS-test/static/css/main.9dfd3842.chunk.css"
+  },
+  {
+    "revision": "3d11c5e5de0b152ab690",
+    "url": "/IBS-test/static/js/2.a847047d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IBS-test/static/js/2.a847047d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6470c88bac13f698ebd",
+    "url": "/IBS-test/static/js/main.3e75a4fd.chunk.js"
+  },
+  {
+    "revision": "c65fb88b30c22767e374",
+    "url": "/IBS-test/static/js/runtime-main.5903ef0d.js"
+  }
+]);
